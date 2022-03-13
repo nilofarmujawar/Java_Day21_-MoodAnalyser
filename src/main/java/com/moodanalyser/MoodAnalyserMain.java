8c16685b8f8859f898ccbb9c9e7ed47aabc0669a
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-14 00:49:34 +0530

    Added: Constructor is used to pass the mood and return the desired mood functionality added.

--- src/main/java/com/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -1,22 +1,33 @@
 package com.moodanalyser;
 
 /**
- * TC 1.2 :- Given “I am in Any Mood” message Should Return HAPPY
- *         - To make the Test case pass analyseMood method need to check for Sad else return HAPPY
+ *  Refactor the code to take the mood message in Constructor
+ * - Note:
+ * - MoodAnalyser will have a message Field
+ * - MoodAnalyser will have 2 Constructors –
+ *   Default - MoodAnalyser() and with
+ *   Parameters – MoodAnalyser(message)
+ * - analyseMood method will change to
+ *   support no parameters and use message
+ *   Field defined for the Class
  */
 
 /**
  * create a class name as MoodAnalyserMain
  */
 public class MoodAnalyserMain {
+    private String message;
     /**
      * create a constructor ,this is default constructor name as MoodAnalyserMain
      */
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyserMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
