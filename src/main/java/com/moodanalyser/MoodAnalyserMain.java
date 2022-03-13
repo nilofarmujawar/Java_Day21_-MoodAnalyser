8f9be22199dee5045d4d353bd3010ffd55e00f36
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-14 01:28:58 +0530

    Added: Handling the exception if user provides mood NULL, functionality added.

--- src/main/java/com/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -1,10 +1,9 @@
 package com.moodanalyser;
 
 /**
- * Repeat TC 1.1 :-
- *
- * Given “I am in Sad Mood” message in Constructor Should  Return SAD
- * To pass this Test Case when calling analyseMood method with no params should return SAD
+ * UC 2 :-
+ * Handle Exception if User Provides Invalid  Mood
+ * - Like NULL
  */
 
 /**
@@ -16,16 +15,22 @@ public class MoodAnalyserMain {
     /**
      * create a default constructor name as MoodAnalyserMain
      */
+
     public MoodAnalyserMain() {
     }
 
     public MoodAnalyserMain(String message) {
-        this.message=message;
+        this.message = message;
     }
+
     public String analyseMood() {
-        if (this.message.contains("Sad"))
-            return "SAD";
-        else
+        try {
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
